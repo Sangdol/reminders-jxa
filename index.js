0fb9201185113180d4da93088ee8d157a459158d
Sanghyun Lee <hammerha@gmail.com>
2019-09-08 12:10:12 +0200

    Add usage

--- index.js
+++ index.js
@@ -19,9 +19,25 @@ function addToReminders(name, dueDate) {
 
 (async () => {
   const args = process.argv.slice(2);
+  const usage = `
+  Usage: node index.js 'name r time and date'
+
+  e.g.,
+  node index.js 'run'
+  node index.js 'run r 11:00'
+  node index.js 'run r tomorrow 11:00'
+  node index.js 'run r 11th 11:00'
+  `;
+
+  if (args.length == 0) {
+    console.log(usage);
+    process.exit();
+  }
+
   const texts = args[0].split(' r ');
   const name = texts[0];
   const date = (texts.length > 1) ? Date.create(texts[1]) : null;
+
   const result = await runJxa(addToReminders, [name, date]);
 
   if (result == 'succeed') {
